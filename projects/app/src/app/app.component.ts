525da68324a0230fa6397c2467957c68032515ca
takayuki_hiraishi <takayuki.hiraishi@gmail.com>
2021-02-04 18:20:56 -0800

    small change

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  public count = 0;
+  countup(){ 
+    console.log('clickoutside');
+    this.count ++; }
 }
--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DirectiveModule } from 'directive';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DirectiveModule
   ],
   providers: [],
   bootstrap: [AppComponent]
